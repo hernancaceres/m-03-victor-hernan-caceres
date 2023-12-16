cb976a273c965c5834a43148e67f40674a4a4076
hernancaceres <harinatha777@hotmail.com>
2023-12-16 17:14:12 -0300

    Navbr y Login ahora son responsivos

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,35 +19,43 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-900 my-3 flex justify-between py-5 px-10 rounded-lg ">
+    <nav className="bg-gray-900 my-3 flex flex-col md:flex-row justify-between py-5 px-4 md:px-10 rounded-lg">
       <Link to="/">
         <h1 className="text-3xl font-bold">VIAJEROS</h1>
       </Link>
-      <ul className="flex gap-x-2 items-center">
+      <ul className="flex flex-col md:flex-row gap-x-2 items-center">
         <li>
-          <Link to="/posts" className="bg-violet-900 px-4 py-2  rounded-sm">
+          <button
+            onClick={() => navigate('/posts')}
+            className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg"
+          >
             Ver Los Posts
-          </Link>
+          </button>
         </li>
         {/* Mostrar el botón "Crear un Post" solo si el usuario está autenticado */}
         {user && (
           <li>
-            <Link to="/create-post" className="bg-violet-900 px-4 py-2 rounded-sm">
+            <button
+              onClick={() => navigate('/create-post')}
+              className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg"
+            >
               Crear un Post
-            </Link>
+            </button>
           </li>
         )}
         {user ? (
           // Mostrar el botón de logout y el mensaje de bienvenida si el usuario está autenticado
           <>
             <li>
-              <Link to={`/profile/${user.id}`} className="bg-violet-900 px-4 py-2 rounded-sm">
+              <button
+                onClick={() => navigate(`/profile/${user.id}`)}
+                className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg">
                 Mi Perfil
-              </Link>
+              </button>
             </li>
 
             <li>
-              <span className="text-white mr-2">Bienvenido {user.username}</span>
+              <span className="text-white mr-2 ">Bienvenido {user.username}</span>
             </li>
             <li>
               {/* Mostrar la imagen del usuario */}
@@ -56,13 +64,13 @@ function Navbar() {
                   src={user.avatarURL}
                   alt="Avatar"
                   className="w-10 h-10 rounded-full object-cover"
-                  style={{  marginRight: '2px' }}
+                  style={{ marginRight: '2px' }}
                 />
               )}
             </li>
 
             <li className="" >
-              <button onClick={logout} className="bg-violet-900 px-4 py-2 rounded-sm ">
+              <button onClick={logout} className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg my-2">
                 Logout
               </button>
             </li>
@@ -71,14 +79,14 @@ function Navbar() {
           // Mostrar los enlaces de login y registro si el usuario no está autenticado
           <>
             <li>
-              <Link to="/login" className="bg-violet-900 px-4 py-2 rounded-sm">
+              <button onClick={() => navigate('/login')} className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg">
                 Login
-              </Link>
+              </button>
             </li>
             <li>
-              <Link to="/register" className="bg-violet-900 px-4 py-2 rounded-sm">
+              <button onClick={() => navigate('/register')} className="bg-violet-900 px-2 md:px-4 py-2 rounded-lg">
                 Register
-              </Link>
+              </button>
             </li>
           </>
         )}
@@ -88,3 +96,98 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
+// import { Link } from "react-router-dom";
+// import { useUser } from "../context/UserContext";
+// import { useNavigate } from "react-router-dom";
+
+// function Navbar() {
+//   const { user, dispatch } = useUser();
+//   const navigate = useNavigate();
+
+//   console.log('usuario navbar:', user);
+
+//   const logout = () => {
+//     // Realiza la acción de logout despachando la acción 'LOGOUT'
+//     dispatch({ type: 'LOGOUT' });
+
+//     // Aquí puedes realizar otras acciones necesarias, como redireccionar a la página de inicio, etc.
+//     // ...
+//     navigate('/login');
+//     console.log('Usuario desconectado');
+//   };
+
+//   return (
+//     <nav className="bg-gray-900 my-3 flex justify-between py-5 px-10 rounded-lg ">
+//       <Link to="/">
+//         <h1 className="text-3xl font-bold">VIAJEROS</h1>
+//       </Link>
+//       <ul className="flex gap-x-2 items-center">
+//         <li>
+//           <Link to="/posts" className="bg-violet-900 px-4 py-2  rounded-sm">
+//             Ver Los Posts
+//           </Link>
+//         </li>
+//         {/* Mostrar el botón "Crear un Post" solo si el usuario está autenticado */}
+//         {user && (
+//           <li>
+//             <Link to="/create-post" className="bg-violet-900 px-4 py-2 rounded-sm">
+//               Crear un Post
+//             </Link>
+//           </li>
+//         )}
+//         {user ? (
+//           // Mostrar el botón de logout y el mensaje de bienvenida si el usuario está autenticado
+//           <>
+//             <li>
+//               <Link to={`/profile/${user.id}`} className="bg-violet-900 px-4 py-2 rounded-sm">
+//                 Mi Perfil
+//               </Link>
+//             </li>
+
+//             <li>
+//               <span className="text-white mr-2">Bienvenido {user.username}</span>
+//             </li>
+//             <li>
+//               {/* Mostrar la imagen del usuario */}
+//               {user && user.avatarURL && (
+//                 <img
+//                   src={user.avatarURL}
+//                   alt="Avatar"
+//                   className="w-10 h-10 rounded-full object-cover"
+//                   style={{  marginRight: '2px' }}
+//                 />
+//               )}
+//             </li>
+
+//             <li className="" >
+//               <button onClick={logout} className="bg-violet-900 px-4 py-2 rounded-sm ">
+//                 Logout
+//               </button>
+//             </li>
+//           </>
+//         ) : (
+//           // Mostrar los enlaces de login y registro si el usuario no está autenticado
+//           <>
+//             <li>
+//               <Link to="/login" className="bg-violet-900 px-4 py-2 rounded-sm">
+//                 Login
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/register" className="bg-violet-900 px-4 py-2 rounded-sm">
+//                 Register
+//               </Link>
+//             </li>
+//           </>
+//         )}
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -1,11 +1,8 @@
-
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
 import axiosInstance from '../components/axios';
 import Cookies from "js-cookie";
 import { useUser } from "../context/UserContext";
-import { Link } from "react-router-dom";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -14,7 +11,7 @@ function LoginForm() {
 
   const [errors, setErrors] = useState([]);
 
-  const { dispatch } = useUser(); // Obtén la función `dispatch` del contexto
+  const { dispatch } = useUser();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,42 +24,30 @@ function LoginForm() {
 
       console.log("Respuesta del servidor:", response);
 
-      // Agrega este console.log para imprimir el token antes de intentar obtenerlo
-      console.log("Token antes de obtenerlo:", response.data && response.data.token);
-
-      // Verifica si la respuesta contiene un campo 'data' y dentro un campo 'token' para obtener el token
       const token = response.data && response.data.token;
 
       console.log("Token obtenido:", token);
 
       if (token) {
-        // Utiliza js-cookie para manejar la cookie
         Cookies.set('token', token);
-
-        console.log('Token almacenado en cookies:', token);
-
-        // Guarda el token en el almacenamiento local
         localStorage.setItem("token", token);
-        // localStorage.setItem('userId', response.data.id);
-        // localStorage.setItem('username', response.data.username);
 
-        // Despacha la acción para establecer el usuario después del inicio de sesión
-        dispatch({ type: 'LOGIN_SUCCESS', payload: { userId: response.data.id, username: response.data.username, avatarURL: response.data.avatarURL, } });
+        dispatch({
+          type: 'LOGIN_SUCCESS',
+          payload: { userId: response.data.id, username: response.data.username, avatarURL: response.data.avatarURL },
+        });
 
         console.log("username LOGIN_SUCCESS en login", response.data.username);
         console.log('Despacho de acción LOGIN_SUCCESS');
         console.log("avatarURL LOGIN_SUCCESS en login", response.data.avatarURL);
 
-        // // Redirige a la página de crear post
         navigate('/create-post');
-
       } else {
         console.error("Token de acceso no válido:", token);
       }
 
     } catch (error) {
       if (error.response && error.response.data && error.response.data.errors) {
-        // Mostrar errores devueltos por el servidor
         setErrors(error.response.data.errors);
       } else {
         console.error('Error en el inicio de sesión:', error.message);
@@ -71,17 +56,19 @@ function LoginForm() {
   };
 
   return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-violet-900">
+      <div className="bg-white max-w-md w-full p-10 rounded-md">
+        <h1 className="text-2xl font-bold mb-4 text-violet-900">Login</h1>
 
-    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-      <div className="bg-violet-900 max-w-md w-full p-10 rounded-md">
-
-        <h1 className="text-2xl font-bold"> Login</h1>
-
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <div>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}
-              className="w-full bg-violet-700 text-white px-4 py-2 rounded-md my-2" />
-            {/* Muestra los errores del campo 'email' */}
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full bg-violet-700 text-white px-4 py-2 rounded-md"
+            />
             {errors.map((error, index) => (
               error.path === 'email' && (
                 <div className="text-red-500" key={index}>
@@ -90,10 +77,15 @@ function LoginForm() {
               )
             ))}
           </div>
+
           <div>
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}
-              className="w-full bg-violet-700 text-white px-4 py-2 rounded-md my-2" />
-            {/* Muestra los errores del campo 'password' */}
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full bg-violet-700 text-white px-4 py-2 rounded-md"
+            />
             {errors.map((error, index) => (
               error.path === 'password' && (
                 <div className="text-red-500" key={index}>
@@ -102,18 +94,140 @@ function LoginForm() {
               )
             ))}
           </div>
-          <button type="submit"  >Iniciar sesión</button>
+
+          <button type="submit" className="bg-violet-900 text-white px-4 py-2 rounded-md hover:bg-violet-700">
+            Iniciar sesión
+          </button>
         </form>
 
-        <p className="flex gap-x-2 justify-between">
-          ¿No tienes una cuenta? <Link to="/register"
-            className="text-withe-500">registrarse</Link>
+        <p className="mt-4 text-violet-900 text-center flex gap-x-2 justify-between">
+          ¿No tienes una cuenta? <Link to="/register" className="text-blue-500">Registrarse</Link>
         </p>
-
       </div>
-
     </div>
   );
 }
 
 export default LoginForm;
+
+
+
+
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import axiosInstance from '../components/axios';
+// import Cookies from "js-cookie";
+// import { useUser } from "../context/UserContext";
+// import { Link } from "react-router-dom";
+
+// function LoginForm() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate();
+
+//   const [errors, setErrors] = useState([]);
+
+//   const { dispatch } = useUser(); // Obtén la función `dispatch` del contexto
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axiosInstance.post("http://localhost:4000/api/login", {
+//         email,
+//         password,
+//       });
+
+//       console.log("Respuesta del servidor:", response);
+
+//       // Agrega este console.log para imprimir el token antes de intentar obtenerlo
+//       console.log("Token antes de obtenerlo:", response.data && response.data.token);
+
+//       // Verifica si la respuesta contiene un campo 'data' y dentro un campo 'token' para obtener el token
+//       const token = response.data && response.data.token;
+
+//       console.log("Token obtenido:", token);
+
+//       if (token) {
+//         // Utiliza js-cookie para manejar la cookie
+//         Cookies.set('token', token);
+
+//         console.log('Token almacenado en cookies:', token);
+
+//         // Guarda el token en el almacenamiento local
+//         localStorage.setItem("token", token);
+//         // localStorage.setItem('userId', response.data.id);
+//         // localStorage.setItem('username', response.data.username);
+
+//         // Despacha la acción para establecer el usuario después del inicio de sesión
+//         dispatch({ type: 'LOGIN_SUCCESS', payload: { userId: response.data.id, username: response.data.username, avatarURL: response.data.avatarURL, } });
+
+//         console.log("username LOGIN_SUCCESS en login", response.data.username);
+//         console.log('Despacho de acción LOGIN_SUCCESS');
+//         console.log("avatarURL LOGIN_SUCCESS en login", response.data.avatarURL);
+
+//         // // Redirige a la página de crear post
+//         navigate('/create-post');
+
+//       } else {
+//         console.error("Token de acceso no válido:", token);
+//       }
+
+//     } catch (error) {
+//       if (error.response && error.response.data && error.response.data.errors) {
+//         // Mostrar errores devueltos por el servidor
+//         setErrors(error.response.data.errors);
+//       } else {
+//         console.error('Error en el inicio de sesión:', error.message);
+//       }
+//     }
+//   };
+
+//   return (
+
+//     <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+//       <div className="bg-violet-900 max-w-md w-full p-10 rounded-md">
+
+//         <h1 className="text-2xl font-bold"> Login</h1>
+
+//         <form onSubmit={handleSubmit}>
+//           <div>
+//             <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}
+//               className="w-full bg-violet-700 text-white px-4 py-2 rounded-md my-2" />
+//             {/* Muestra los errores del campo 'email' */}
+//             {errors.map((error, index) => (
+//               error.path === 'email' && (
+//                 <div className="text-red-500" key={index}>
+//                   {error.msg}
+//                 </div>
+//               )
+//             ))}
+//           </div>
+//           <div>
+//             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}
+//               className="w-full bg-violet-700 text-white px-4 py-2 rounded-md my-2" />
+//             {/* Muestra los errores del campo 'password' */}
+//             {errors.map((error, index) => (
+//               error.path === 'password' && (
+//                 <div className="text-red-500" key={index}>
+//                   {error.msg}
+//                 </div>
+//               )
+//             ))}
+//           </div>
+//           <button type="submit"  >Iniciar sesión</button>
+//         </form>
+
+//         <p className="flex gap-x-2 justify-between">
+//           ¿No tiene una cuenta? <Link to="/register"
+//             className="text-withe-500">registrarse</Link>
+//         </p>
+
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default LoginForm;
